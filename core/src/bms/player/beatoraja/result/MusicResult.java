596fbb5ad041716b790e585838c53400c4ea2a7a
rian-eimu <92227961+rian-eimu@users.noreply.github.com>
2026-06-24 23:52:19 +0900

    複数のIRに接続した際に発生するデータ送信不具合の修正

--- core/src/bms/player/beatoraja/result/MusicResult.java
+++ core/src/bms/player/beatoraja/result/MusicResult.java
@@ -120,6 +120,7 @@ public class MusicResult extends AbstractResult {
 		if (ir.length > 0 && resource.getPlayMode().mode == BMSPlayerMode.Mode.PLAY && !resource.isFreqOn()) {
 			state = STATE_IR_PROCESSING;
 			
+			List<IRSendStatus> addedScores = new ArrayList<IRSendStatus>();
         	for(IRStatus irc : ir) {
     			boolean send = resource.isUpdateScore() && !resource.isForceNoIRSend();
     			switch(irc.config.getIrsend()) {
@@ -140,7 +141,9 @@ public class MusicResult extends AbstractResult {
                         continue;
                     }
                     
-    				main.irSendStatus.add(new IRSendStatus(irc.connection, resource.getSongdata(), newscore, resource.getReplayData()));
+                    IRSendStatus status = new IRSendStatus(irc.connection, resource.getSongdata(), newscore, resource.getReplayData());
+    				main.irSendStatus.add(status);
+					addedScores.add(status);
     			}
         	}
 			
@@ -148,10 +151,7 @@ public class MusicResult extends AbstractResult {
 				int irsend = 0;
 				boolean succeed = true;
 				List<IRSendStatus> removeIrSendStatus = new ArrayList<IRSendStatus>();
-				List<IRSendStatus> scores = new ArrayList<IRSendStatus>();
-				if (!main.irSendStatus.isEmpty()) {
-					scores = main.irSendStatus.subList(main.irSendStatus.size() - ir.length, main.irSendStatus.size());
-				}
+				List<IRSendStatus> scores = addedScores;
 
 				for (IRSendStatus irc : scores) {
 					try {
